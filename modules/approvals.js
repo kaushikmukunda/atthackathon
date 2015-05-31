18951c31299b43beaae11f9402b4f91b499b1ffa
kaushikmukunda <kmukunda@qualcomm.com>
2015-05-30 19:02:44 -0700

    Initial commit

---
+++ modules/approvals.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var http = require('http');
+var Promise = require('bluebird');
+
+var exports = module.exports = {};
+
+exports.getApprovals = function () {
+  return new Promise(function (resolve, reject) {
+  });
+};
